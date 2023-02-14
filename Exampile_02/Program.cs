8b342786770059af1d01956026dc630381c7826c
Yaroslav <tarasdmb2008@mail.ru>
2023-02-14 19:49:50 +0500

    second examle

---
+++ Exampile_02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter a third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+System.Console.WriteLine($"Maksimal number: {max}");
